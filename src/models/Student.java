98c7a63e7d029a9b5cc9f5b17da2176802c738a9
AntiDroid <AntiDroid@users.noreply.github.com>
2017-03-31 12:07:03 +0200

    Fix: Logout wenn keine Session --> crash

--- src/models/Student.java
+++ src/models/Student.java
@@ -2,6 +2,8 @@ package models;
 
 public class Student extends Client{
 
+	private static final long serialVersionUID = 1L;
+	
 	public Student(String bn, String vn, String nn, String pw) {
 		super(bn, vn, nn, pw);
 	}
